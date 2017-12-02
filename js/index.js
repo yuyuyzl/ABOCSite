133d9b2a02085f7ef3fd0d5ec13ecd9aabe614cd
yuyuyzl <yuzhongliang22@163.COM>
2017-12-02 13:08:38 +0800

    hero video area

--- js/index.js
+++ js/index.js
@@ -1,13 +1,26 @@
+var navNeedFade=false;
 $(document).ready(function () {
+    $("#hero").height($("herovideo").height());
     $(window).scroll(function () {
-        console.log($(window).scrollTop());
-        if($(window).scrollTop()>300){
-            $("#navmain").addClass("bg-dark");
-            $("#navmain").removeClass("bg-transparent");
-        }else{
-            $("#navmain").addClass("bg-transparent");
-            $("#navmain").removeClass("bg-dark");
+        if(($(window).scrollTop()>$("#hero").innerHeight())!=navNeedFade) {
+            navNeedFade=($(window).scrollTop()>$("#hero").innerHeight());
+            if ($(window).scrollTop() > $("#hero").innerHeight()) {
+                $("#navmain").hide();
+                $("#navmain").removeClass("bg-transparent");
+                $("#navmain").removeClass("navbar-dark");
+                $("#navmain").addClass("bg-white");
+                $("#navmain").addClass("navbar-light");
+                $("#navmain").fadeIn("fast");
+            } else {
+                $("#navmain").fadeOut("fast",function () {
+                    $("#navmain").removeClass("bg-white");
+                    $("#navmain").removeClass("navbar-light");
+                    $("#navmain").addClass("bg-transparent");
+                    $("#navmain").addClass("navbar-dark");
+                    $("#navmain").show();
+                });
+            }
         }
-
     });
+
 });
